2c69b7649bbc0ef7fe4fa80811bf58ee04e431b7
Bora Yalciner <yalciner.bora@metu.edu.tr>
2021-05-19 20:06:04 +0300

    Starting to implement Practical Path Guiding Tracer

--- Source/TracerCUDA/DTree.cuh
+++ Source/TracerCUDA/DTree.cuh
@@ -1,4 +1,46 @@
 ﻿#pragma once
 
-// DTree Implementation
-// 
+/**
+
+DTree Implementation
+
+*/ 
+
+#include "RayLib/Vector.h"
+
+#include "DeviceMemory.h"
+
+struct DTreeNode
+{
+    enum NodeOrder
+    {
+        BOTTOM_LEFT     = 0,
+        BOTTOM_RIGHT    = 1,
+        TOP_LEFT        = 2,
+        TOP_RIGHT       = 3
+    };
+
+    Vector4ui   childIndex;
+    Vector4f    cdfs;
+};
+
+class DTree
+{
+    private:
+        DeviceMemory    treeNodeMemory;
+        DTreeNode*      dTreeNodes;
+
+    protected:
+
+    public:
+        // Constructors & Destructor
+                        DTree();
+                        DTree(const DTree&) = delete;
+                        DTree(DTree&&) = default;
+        DTree&          operator=(const DTree&) = delete;
+        DTree&          operator=(DTree&&) = default;
+                        ~DTree() = default;
+
+        // Members
+        
+};
