80816092effae4e0c1aa8aee7a6b16e16660def9
Bora Yalciner <yalcinerbora@live.com>
2021-11-29 01:14:15 +0300

    Found the most suitable font finally changed to icomoon.ttf generated header file for it mocking some UI

--- Source/VisorGL/VisorGUI.cpp
+++ Source/VisorGL/VisorGUI.cpp
@@ -6,42 +6,53 @@
 #include <glfw/glfw3.h>
 
 #include "RayLib/FileSystemUtility.h"
-#include "OpenFontIconsTable.h"
+
+// Icon Font UTF Definitions
+#include "IcoMoonFontTable.h"
 
 VisorGUI::VisorGUI(GLFWwindow* window)
-    : bottomBarOn(false)
-    , topBarOn(false)
+    : bottomBarOn(true)
+    , topBarOn(true)
 {
     init = true;
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+
+    constexpr float INITIAL_SCALE = 1.05f;
 
     // Get Scale Info
     float x, y;
     glfwGetWindowContentScale(window, &x, &y);
     assert(x == y);
 
+    x *= INITIAL_SCALE;
+    constexpr float PIXEL_SIZE = 13;
+    float scaledPixelSize = std::roundf(PIXEL_SIZE * x);
+
     // Set Scaled Fonts
     ImGuiIO& io = ImGui::GetIO();
     io.Fonts->Clear();
     ImFontConfig config;
-    config.SizePixels = std::roundf(14.0f * x);
+    config.SizePixels = scaledPixelSize;
     config.PixelSnapH = false;
     config.MergeMode = false;
     std::string monoTTFPath = Utility::MergeFileFolder("Fonts", "VeraMono.ttf");
     io.Fonts->AddFontFromFileTTF(monoTTFPath.c_str(),
                                  config.SizePixels,
                                  &config);
-    // Font Awesome
+    // Icomoon
     config.MergeMode = true;
-    config.PixelSnapH = false;
-    config.GlyphMinAdvanceX = config.SizePixels;
-    config.GlyphMaxAdvanceX = config.SizePixels;
-    static const ImWchar icon_ranges[] = {ICON_MIN_OFI, ICON_MAX_OFI, 0};
-    std::string ofiTTFPath = Utility::MergeFileFolder("Fonts", FONT_ICON_FILE_NAME_OFI);
+    config.PixelSnapH = true;
+    config.GlyphMinAdvanceX = scaledPixelSize;
+    config.GlyphMaxAdvanceX = scaledPixelSize;
+    config.OversampleH = config.OversampleV = 1;
+    config.GlyphOffset = ImVec2(0, 4);
+    static const ImWchar icon_ranges[] = {ICON_MIN_ICOMN, ICON_MAX_ICOMN, 0};
+    std::string ofiTTFPath = Utility::MergeFileFolder("Fonts", FONT_ICON_FILE_NAME_ICOMN);
     io.Fonts->AddFontFromFileTTF(ofiTTFPath.c_str(),
-                                 config.SizePixels * 0.90f,
+                                 scaledPixelSize,
                                  &config, icon_ranges);
+
     // ImGUI Dark
     ImGui::StyleColorsDark();
     // Scale everything according to the DPI
@@ -95,11 +106,45 @@ void VisorGUI::Render(GLuint, const Vector2i&)
         {
             if(ImGui::BeginMenuBar())
             {
-                ImGui::Button(ICON_OFI_PLAY_CIRCLE);
-                ImGui::Button(ICON_OFI_PAUSE_CIRCLE);
-                ImGui::Button(ICON_OFI_STOP_CIRCLE);
 
-                ImGui::Text("Happy status bar");
+                ImGui::Text("1280 x 720");
+                ImGui::Separator();
+
+                ImGui::Text("10.3322M paths / sec");
+                ImGui::Separator();
+
+                ImGui::Text("300 spp");
+                ImGui::Separator();
+
+                ImGui::Text("Rendering test.json...");
+
+                ImGui::Separator();
+
+                ImGui::Button(ICON_ICOMN_ARROW_LEFT);
+                if(ImGui::IsItemHovered() && GImGui->HoveredIdTimer > 2)
+                {
+                    ImGui::BeginTooltip();
+                    ImGui::Text("Prev Frame");
+                    ImGui::EndTooltip();
+                }
+
+                ImGui::Button(ICON_ICOMN_ARROW_RIGHT);
+                if(ImGui::IsItemHovered() && GImGui->HoveredIdTimer > 2)
+                {
+                    ImGui::BeginTooltip();
+                    ImGui::Text("Next Frame");
+                    ImGui::EndTooltip();
+                }
+
+                ImGui::Separator();
+
+                ImGui::Button(ICON_ICOMN_STOP2);
+                ImGui::Button(ICON_ICOMN_PAUSE2);
+                ImGui::Button(ICON_ICOMN_PLAY3);
+
+
+
+
                 ImGui::EndMenuBar();
             }
         }
